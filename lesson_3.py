e93cdd1a1faa0943658774028556c2eb00cb6eba
AlitaVitalii <98693777+AlitaVitalii@users.noreply.github.com>
2022-02-02 21:14:50 +0200

    py5

---
+++ lesson_3.py
@@ -0,0 +1,37 @@
+# import math
+# a = 3**5
+# print(a)
+# print(math.sqrt(a))
+
+# print(2!=2)
+
+# x=5
+# print(0<=x<=10)  #print (0<=x and x<=10)
+
+# print(1 or 2)
+# print(1 and 2)
+
+# a = 260
+# b = 260
+# print(a is b)
+# print(a is not b)
+
+# print(0.1+0.2)
+
+
+# a = input("Введите первое число ")
+# b = input("Введите второе число ")
+# if int(a)+int(b)==7:
+#     print ("ok")
+# elif int(a)+int(b)>7:
+#     print("ввели большее число")
+# elif int(a)+int(b)<7:
+#     print("ввели меньшее число")
+# else:
+#     print("No")
+
+# print(x for x in range (100))
+
+
+number = 11
+print(number if number <= number)
