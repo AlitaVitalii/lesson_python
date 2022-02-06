7ef42bd41f66d0b7f41bddc77b29372cdf4f2c92
AlitaVitalii <98693777+AlitaVitalii@users.noreply.github.com>
2022-02-06 16:11:32 +0200

    py7

---
+++ lesson_4_for_and_while.py
@@ -0,0 +1,59 @@
+# for number in [0, 1, 2, 3, 4]:
+#     print(number)
+#
+# for num in range(10, 50, 10):
+#     print(num)
+# else:
+#     print("числа закончились")
+#
+# print(len(list(range(10,50)))) #len - длина списка
+
+# mystr = "привет Пайтон!"
+# line_str = "-" * len(mystr)
+# print(f'{mystr}\n{line_str}')
+
+
+
+# for num in range(5):
+#     if num == 3:
+#         break
+#     else:
+#         print(num)
+# else:
+#     print("числа закончились")
+#
+
+
+# import random
+# print(random.randrange(3, 9))
+# print(random.random())
+
+
+# import time
+# print(time.time())
+# time.sleep(5)
+# print(time.time())
+
+
+
+# i = 0
+# while i < 5:
+#     if i == 3:
+#         continue   #получается замкнутый цикл, нужно +1 поставить до континью
+#     print(i)
+#     i += 1
+# else:
+#     print("the end")
+
+
+# import time
+# end_time = time.time() + 20
+# while time.time() < end_time:
+#     time.sleep(5)
+#     print("Прошло 5 сек")
+# else:
+#     print("Alarm")
+
+
+def generator():
+    for num in range(10):
