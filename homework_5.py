656b2844b211d3ac9de8bbe14c03691207e9d3e6
AlitaVitalii <98693777+AlitaVitalii@users.noreply.github.com>
2022-02-10 15:18:45 +0200

    py9

---
+++ homework_5.py
@@ -0,0 +1,16 @@
+num = abs(int(input("введите число: ")))
+n = 1
+step = 0
+mylist = []
+
+while n <= num:
+    step += 1
+    n = step**2
+    # mylist.append(n)
+    if n <= num:  # можно обойтись без if, выводя список применив del mylist[-1]
+        mylist.append(n)
+
+# del mylist[-1]
+print(f'все квадраты натуральных чисел: {mylist}')
+
+
