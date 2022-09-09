f08dcbaa0b0b85c7b3c17bea2f9399b4d51c9015
AlitaVitalii <98693777+AlitaVitalii@users.noreply.github.com>
2022-09-09 11:07:03 +0300

    py15

---
+++ python_book/01/2.1.py
@@ -0,0 +1,14 @@
+message = 'hello worlld'
+print(message)
+print(message.title())
+print(message.upper())
+print()
+
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = f'{first_name} {last_name}'
+print(full_name.title())
+print( f'Hello,\t{full_name.title()}')
+
+print()
+print(message.strip())
---
+++ python_book/01/2.3.py
@@ -0,0 +1,8 @@
+name = 'vitalii'
+print(f'Hello {name.upper()}, would you like to learn some Python today')
+
+print()
+name = "Albert Einstein"
+message = '"A person who never made a mistake never tried anything new."'
+print(f'{name} once said {message}')
+print(len(message))
