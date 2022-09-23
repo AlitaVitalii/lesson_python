a4cc36f02efa50d8c4587c59c739cb19e045db83
AlitaVitalii <98693777+AlitaVitalii@users.noreply.github.com>
2022-09-23 17:21:28 +0300

    21spt

--- python_book/08/greeter.py
+++ python_book/08/greeter.py
@@ -2,3 +2,10 @@ def greet_user(user):
     print((f'Hello, {user.title()}!'))
 
 greet_user('vitalii')
+
+while True:
+    print("\nPlease tell me your name:")
+    f_name = input("First name:")
+    l_name = input("Last name:")
+    formatted_name = greet_user(f_name, l_name)
+    print(f'\nHello, {formatted_name}')
