ceb1b724cdbdfba9139cdb8b2b7c27fa8a349ab0
AlitaVitalii <98693777+AlitaVitalii@users.noreply.github.com>
2022-10-08 00:22:35 +0300

    07oct

--- python_basic_28.09.2022/dz4.py
+++ python_basic_28.09.2022/dz4.py
@@ -1,13 +1,19 @@
-num_ = int(input("vvedite chislo: "))
-i = 1
+num_1 = int(input("Введіть нижню межу діапазону для пошуку простих чисел: "))
+num_2 = int(input('Введіть верхню межу діапазону для пошуку простих чисел: '))
 s = 0
-while i <= num_:
+my_list = []
+while num_1 <= num_2:
     a = 1
-    while a <= num_:
-        if i % a == 0:
+    while a <= num_2:
+        if num_1 % a == 0:
             s += 1
         a += 1
     if s == 2:
-        print(i)
+        my_list.append(num_1)
     s = 0
-    i += 1
+    num_1 += 1
+
+print('\nПрості числа: ', my_list)
+# for i in my_list:
+#     print(i)
+print('Знайдено простих чисел: ', len(my_list))
