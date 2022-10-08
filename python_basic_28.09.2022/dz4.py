407c7bfa77decbfa55ea0ec4f60c3b4c2ec97a53
AlitaVitalii <98693777+AlitaVitalii@users.noreply.github.com>
2022-10-08 09:42:44 +0300

    07oct

--- python_basic_28.09.2022/dz4.py
+++ python_basic_28.09.2022/dz4.py
@@ -1,19 +1,26 @@
 num_1 = int(input("Введіть нижню межу діапазону для пошуку простих чисел: "))
 num_2 = int(input('Введіть верхню межу діапазону для пошуку простих чисел: '))
 s = 0
-my_list = []
+# my_list = []
+sum_ = 0
+print()
+print('Прості числа:')
 while num_1 <= num_2:
-    a = 1
+    a = 2
     while a <= num_2:
         if num_1 % a == 0:
             s += 1
+        if s > 1:
+            break
         a += 1
-    if s == 2:
-        my_list.append(num_1)
+    if s == 1:
+        sum_ += 1
+        print(num_1)
     s = 0
     num_1 += 1
 
-print('\nПрості числа: ', my_list)
+# print('\nПрості числа: ', my_list)
 # for i in my_list:
 #     print(i)
-print('Знайдено простих чисел: ', len(my_list))
+# print('Знайдено простих чисел: ', len(my_list))
+print('Знайдено простих чисел: ', sum_)
