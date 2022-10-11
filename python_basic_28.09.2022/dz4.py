d9e3d2ba705d74f676cb87600e0b7f11012aea9b
AlitaVitalii <98693777+AlitaVitalii@users.noreply.github.com>
2022-10-11 21:52:30 +0300

    11oct

--- python_basic_28.09.2022/dz4.py
+++ python_basic_28.09.2022/dz4.py
@@ -1,7 +1,7 @@
 num_1 = int(input("Введіть нижню межу діапазону для пошуку простих чисел: "))
 num_2 = int(input('Введіть верхню межу діапазону для пошуку простих чисел: '))
 sum_delitel = 0
-sum_numbers = 0
+counter_prime = 0
 print('Прості числа:')
 
 while num_1 <= num_2:
@@ -13,9 +13,9 @@ while num_1 <= num_2:
             break
         delitel += 1
     if sum_delitel == 1:
-        sum_numbers += 1
+        counter_prime += 1
         print(num_1)
     sum_delitel = 0
     num_1 += 1
 
-print('Знайдено простих чисел: ', sum_numbers)
+print('Знайдено простих чисел: ', counter_prime)
--- python_basic_28.09.2022/dz7_v2.py
+++ python_basic_28.09.2022/dz7_v2.py
@@ -1,10 +1,11 @@
 # text = "Розум ЦЕ здатність адаптуватися до змін."
 text = input("Введіть текст: ")
 counter = 0
-# new_text = ""
+new_text = ""
 for t in text:
     counter += 1
     if counter % 2 == 0:
-        print(t.lower(), end="")
+        new_text += (t.lower())
     else:
-        print(t.upper(), end="")
+        new_text += (t.upper())
+print(new_text)
