719fbc942698873c641a80e6c731d7e383ea9bd2
AlitaVitalii <98693777+AlitaVitalii@users.noreply.github.com>
2022-10-11 22:28:05 +0300

    11oct

--- python_basic_28.09.2022/dz7_v2.py
+++ python_basic_28.09.2022/dz7_v2.py
@@ -5,7 +5,7 @@ new_text = ""
 for t in text:
     counter += 1
     if counter % 2 == 0:
-        new_text += (t.lower())
+        new_text += t.lower()
     else:
-        new_text += (t.upper())
+        new_text += t.upper()
 print(new_text)
