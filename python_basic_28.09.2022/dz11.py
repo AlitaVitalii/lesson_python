8d3e5e1f0cc5daa4a91ee8ab0f4b2bab6366014e
AlitaVitalii <98693777+AlitaVitalii@users.noreply.github.com>
2022-10-18 20:04:30 +0300

    18oct

---
+++ python_basic_28.09.2022/dz11.py
@@ -0,0 +1,13 @@
+text_1 = input('Введіть перший фрагмент тексту: ')
+text_2 = input('Введіть другий фрагмент тексту: ')
+
+list_1 = sorted([i.lower() for i in text_1 if i.isalpha()])
+list_2 = sorted([i.lower() for i in text_2 if i.isalpha()]
+
+if list_1 == list_2 and text_1 != text_2:
+    print('Тексти є анаграмами')
+elif text_2 == text_1:
+    print('Тексти однакові')
+else:
+    print('Тексти не є анаграмами')
+
