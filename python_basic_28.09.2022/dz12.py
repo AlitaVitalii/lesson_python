b57e6b52f0211cb0376cc704f7b7f29797dcdc83
AlitaVitalii <98693777+AlitaVitalii@users.noreply.github.com>
2022-10-19 19:14:16 +0300

    19oct

---
+++ python_basic_28.09.2022/dz12.py
@@ -0,0 +1,10 @@
+text = input('Введіть текст: ')
+
+my_list = [i.lower() for i in text if i.isalpha()]
+my_list_set_sorted = sorted(list(set(my_list)))
+
+for letter_unique in my_list_set_sorted:
+    sum_letter = my_list.count(letter_unique)
+    print(f'"{letter_unique}": {sum_letter}')
+
+
