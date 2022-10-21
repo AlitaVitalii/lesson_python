2c8140f172b12bb0bc5b90e3da8c98ae32368462
AlitaVitalii <98693777+AlitaVitalii@users.noreply.github.com>
2022-10-21 21:40:49 +0300

    21oct

--- python_basic_28.09.2022/dz14.py
+++ python_basic_28.09.2022/dz14.py
@@ -4,9 +4,13 @@ d = {
     "punishment": ["malum", "multa"]
 }
 
-set_values = ()
+set_values = set()
 for v_01 in d.values():
     for v_02 in v_01:
-        set_values += [v_02]
+        set_values.add(v_02)
 
-print(set_values)
+for i_set in set_values:
+    print(f'{i_set.upper()}:')
+    for key, values in d.items():
+        if i_set in values:
+            print(f'- {key}')
