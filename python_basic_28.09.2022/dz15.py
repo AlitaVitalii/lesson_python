0b56619848643dfc478be1f8792eb4c6a5b748bf
AlitaVitalii <98693777+AlitaVitalii@users.noreply.github.com>
2022-10-23 16:44:09 +0300

    23oct

--- python_basic_28.09.2022/dz15.py
+++ python_basic_28.09.2022/dz15.py
@@ -1,7 +1,5 @@
 num_1 = int(input("Введіть нижню межу діапазону для пошуку простих чисел: "))
 num_2 = int(input('Введіть верхню межу діапазону для пошуку простих чисел: '))
-counter_prime = 0
-print('Прості числа:')
 
 
 def is_prime(num):
@@ -13,6 +11,8 @@ def is_prime(num):
     return True
 
 
+print('Прості числа:')
+counter_prime = 0
 while num_1 <= num_2:
     if is_prime(num_1):
         print(num_1)
