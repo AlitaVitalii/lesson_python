e8749fa16c8324aa65369b5f4fcac03226b360ab
AlitaVitalii <98693777+AlitaVitalii@users.noreply.github.com>
2022-10-29 21:10:15 +0300

    29oct

--- python_basic_28.09.2022/dz18.py
+++ python_basic_28.09.2022/dz18.py
@@ -1,13 +1,17 @@
-text = 'qwertyu'
+text = input("Введіть текст: ")
 
-count = 1
-list_text = list(text)
+count = 0
+half_len = int(len(text) / 2)
 
-
-for i in range(int(len(list_text)/2)):
-    # if len(list_text) % 2 == 0:
-    print(" " * (int(len(list_text)/2-count)), text[int(len(list_text)/2-count): int(len(list_text)/2+count)])
+for i in range(half_len + 1):
     count += 1
+    if len(text) % 2 == 0 and count <= half_len:
+        print(" " * (half_len - count), text[half_len - count: half_len + count])
+    elif len(text) % 2 > 0:
+        if count == 1:
+            print(" " * (half_len - count + 1), text[half_len])
+        else:
+            print(" " * (half_len - count + 1), text[half_len - count + 1: half_len + count])
 
 
 
