1b6205c1239bc06899996f97f4753fcb55bc56b9
AlitaVitalii <98693777+AlitaVitalii@users.noreply.github.com>
2022-10-30 10:58:36 +0200

    30oct

---
+++ python_basic_28.09.2022/dz22.py
@@ -0,0 +1,16 @@
+
+text = input('Введіть дані: ')
+list_01 = [int(i) for i in text.split(',')]
+
+counter = 0
+max_count = 0
+
+for i in range(len(list_01)-1):
+    if list_01[i] % 2 == 0 and list_01[i+1] % 2 == 0 and list_01[i] != list_01[i+1]:
+        counter += 1
+        if max_count <= counter:
+            max_count = counter + 1
+    else:
+        counter = 0
+
+print('Найбільше парних чисел підряд: ', max_count)
