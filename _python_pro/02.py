03b844392e960f8e35c7edf32a765cfa763226d9
AlitaVitalii <98693777+AlitaVitalii@users.noreply.github.com>
2022-12-27 19:48:58 +0200

    27dec

--- _python_pro/02.py
+++ _python_pro/02.py
@@ -1,1 +1,12 @@
-a = 1
+def func(s):
+    result = []
+    # my_list = s.split()
+    for i in s.split():
+        if len(i) >= 5:
+            result.append(i[::-1])
+        else:
+            result.append(i)
+    return ' '.join(result)
+
+
+print(func('helloo word'))
